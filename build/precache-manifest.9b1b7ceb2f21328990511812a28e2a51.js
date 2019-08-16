4ae258b6956fecdf8f67b4294d46d15e9f30c912
hajar <hajar.kian@yahoo.com>
2019-08-16 11:19:44 +0430

    in 8_16AtTime11_19

---
+++ build/precache-manifest.9b1b7ceb2f21328990511812a28e2a51.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "84f49632b1d3dd0f400f4b939bee08cf",
+    "url": "/ReactDemo/static/media/small_logo_tr.84f49632.png"
+  },
+  {
+    "revision": "661be74eacdfa07c469873cc99a117dc",
+    "url": "/ReactDemo/static/media/logo_tr.661be74e.png"
+  },
+  {
+    "revision": "a82ae06ee47d91df156c",
+    "url": "/ReactDemo/static/js/runtime~main.a82ae06e.js"
+  },
+  {
+    "revision": "ec462778da6034e8bc40",
+    "url": "/ReactDemo/static/js/main.ec462778.chunk.js"
+  },
+  {
+    "revision": "882f7a574efe9c488435",
+    "url": "/ReactDemo/static/js/1.882f7a57.chunk.js"
+  },
+  {
+    "revision": "ec462778da6034e8bc40",
+    "url": "/ReactDemo/static/css/main.ea642130.chunk.css"
+  },
+  {
+    "revision": "882f7a574efe9c488435",
+    "url": "/ReactDemo/static/css/1.6c4c4fdf.chunk.css"
+  },
+  {
+    "revision": "75fad9e0ca1b3ec5bd98009aa9d762fe",
+    "url": "/ReactDemo/index.html"
+  }
+];
